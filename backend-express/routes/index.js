12aaefea492e538d5c0bb86332848246d6a20f77
haikalnuril <haikal.nuril23@gmail.com>
2024-06-28 06:43:04 +0700

    feat: login

--- backend-express/routes/index.js
+++ backend-express/routes/index.js
@@ -7,11 +7,15 @@ const router = express.Router();
 //import register controller
 const registerController = require('../controllers/RegisterController');
 
+const loginController = require('../controllers/LoginController');
+
 //import validate register
-const { validateRegister } = require('../utils/validators/auth');
+const { validateRegister, validateLogin } = require('../utils/validators/auth');
 
 //define route for register
 router.post('/register', validateRegister, registerController.register);
+
+router.post('/login', validateLogin, loginController.login);
 
 //export router
 module.exports = router
