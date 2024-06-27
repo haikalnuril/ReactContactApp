043d341ad8075271ee2aa80d3a79d7007f8e6559
haikalnuril <haikal.nuril23@gmail.com>
2024-06-28 06:49:56 +0700

    feat: read Users

--- backend-express/routes/index.js
+++ backend-express/routes/index.js
@@ -4,18 +4,26 @@ const express = require('express')
 //init express router
 const router = express.Router();
 
+//import verifyToken
+const verifyToken = require('../middlewares/auth');
+
 //import register controller
 const registerController = require('../controllers/RegisterController');
 
 const loginController = require('../controllers/LoginController');
 
+const userController = require('../controllers/UserController');
+
 //import validate register
 const { validateRegister, validateLogin } = require('../utils/validators/auth');
+const { verify } = require('jsonwebtoken');
 
 //define route for register
 router.post('/register', validateRegister, registerController.register);
 
 router.post('/login', validateLogin, loginController.login);
+
+router.get('/admin/users', verifyToken, userController.findUsers);
 
 //export router
 module.exports = router
