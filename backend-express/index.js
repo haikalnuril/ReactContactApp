bd5a1d44938019bb6911ee1dcee6a78f0ee36bda
haikalnuril <haikal.nuril23@gmail.com>
2024-06-27 16:49:57 +0700

    feat: register

--- backend-express/index.js
+++ backend-express/index.js
@@ -1,9 +1,19 @@
-const express = require ('express')
+//import express
+const express = require('express')
 
-const cors = require ('cors')
+//import CORS
+const cors = require('cors')
 
+//import bodyParser
+const bodyParser = require('body-parser')
+
+//import router
+const router = require('./routes')
+
+//init app
 const app = express()
 
+//use cors
 app.use(cors())
 
 //use body parser
@@ -12,12 +22,18 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
+//define port
 const port = 3000;
 
-app.get('/', (req, res) =>{
-    res.send('hello world!')
+//route
+app.get('/', (req, res) => {
+  res.send('Hello World!')
 })
 
+//define routes
+app.use('/api', router);
+
+//start server
 app.listen(port, () => {
-    console.log(`Server started on port ${port}`)
+    console.log(`Server started on port ${port}`);
 })
