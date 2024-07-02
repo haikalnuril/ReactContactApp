0926521f79acbb78f64a8c58139ee4483286e87e
haikalnuril <haikal.nuril23@gmail.com>
2024-07-02 17:00:15 +0700

    feat: view auth login and register

---
+++ frontend-react/src/routes/index.jsx
@@ -0,0 +1,34 @@
+import React, { useContext } from 'react';
+
+import {AuthContext} from '../context/AuthContext';
+
+import {Routes, Route, Navigate } from 'react-router-dom';
+
+import Home from '../views/home/index.jsx';
+
+import Register from '../views/auth/register.jsx';
+
+import Login from '../views/auth/login.jsx';
+
+export default function AppRoutes() {
+
+    //destructure context "isAuthenticated"
+    const { isAuthenticated } = useContext(AuthContext);
+
+    return (
+        <Routes>
+            {/* route "/" */}
+            <Route path="/" element={<Home />} />
+
+            {/* route "/register" */}
+            <Route path="/register" element={
+                isAuthenticated ? <Navigate to="/admin/dashboard" replace /> : <Register />
+            } />
+
+            {/* route "/login" */}
+            <Route path="/login" element={
+                isAuthenticated ? <Navigate to="/admin/dashboard" replace /> : <Login />
+            } />
+        </Routes>
+    );
+}
